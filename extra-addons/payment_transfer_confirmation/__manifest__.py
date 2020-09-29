a4b0f0a49d451fbade697bb4450ec9ab8fcdbfde
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2020-09-29 18:27:54 +0000

    Wire Transter: confirm order, capture transaction PEN-25

---
+++ extra-addons/payment_transfer_confirmation/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Order Confirmation for Wire Transfer',
+    'category': 'Accounting/Payment',
+    'version': '1.0',
+    'description': "Auto confirm order on payment confiramtion.",
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'OPL-1',
+    'depends': ['payment_transfer', 'sale'],
+    'auto_install': False,
+    'application': False,
+}
