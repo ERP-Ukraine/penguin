585e500fc274df464c55c2afaae7a3a8ede00f83
Nikita Borovik <nick.borovik@gmail.com>
2020-12-01 11:03:35 +0000

    Merged in PEN-39_compute_rrp (pull request #35)
    [IMP] sale_penguin compute RRP based on base pricelist PEN-39
    
    * [IMP] sale_penguin compute RRP based on base pricelist PEN-39
    
    Approved-by: Bohdan Lisnenko

--- extra-addons/sale_penguin/__manifest__.py
+++ extra-addons/sale_penguin/__manifest__.py
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Penguin Sales',
-    'version': '1.3',
+    'version': '1.4',
     'category': 'Sales/Sales',
     'summary': 'Sales internal machinery',
     'installable': True,
     'auto_install': True,
-
     'depends': [
-        'sale',
         'product_penguin',
+        'sale',
     ],
 
     'data': [
         'data/mail_data.xml',
         'report/sale_report_templates.xml',
+        'views/res_partner_views.xml',
+        'views/res_users_view.xml',
+        'views/sale_order_templates.xml',
         'views/sale_views.xml',
         'views/variant_templates.xml',
-        'views/sale_order_templates.xml',
-        'views/res_users_view.xml',
-        'views/res_partner_views.xml',
     ]
 }
--- extra-addons/sale_penguin/models/sale_order_line.py
+++ extra-addons/sale_penguin/models/sale_order_line.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -20,12 +21,24 @@ class SaleOrderLine(models.Model):
             )
             line.penguin_net_price_pc = price_info['total_excluded']
 
+    @api.depends('order_id.pricelist_id')
     def _compute_penguin_rrp_pc(self):
         for line in self:
-            order_currency = line.order_id.pricelist_id.currency_id
-            domain = [('currency_id', '=', order_currency.id), ('selectable', '=', True)]
-            base_pl = self.env['product.pricelist'].search(domain, limit=1)
             product = line.product_id
-            if base_pl:
-                product = product.with_context(pricelist=base_pl.id)
-            line.penguin_rrp_pc = product.price
+            pricelist_id = line.order_id.pricelist_id
+            if not pricelist_id:
+                line.penguin_rrp_pc = product.lst_price
+                continue
+            products_qty_partner = [(
+                product, line.product_uom_qty, line.order_partner_id
+            )]
+            results = pricelist_id._compute_price_rule(products_qty_partner)
+            price, suitable_rule = results[product.id]
+            if suitable_rule:
+                item = self.env['product.pricelist.item'].browse(suitable_rule)
+                if item.pricelist_id == pricelist_id and item.base == 'pricelist':
+                    line.penguin_rrp_pc = product.with_context(pricelist=item.base_pricelist_id.id).price
+                else:
+                    line.penguin_rrp_pc = product.lst_price
+            else:
+                line.penguin_rrp_pc = product.lst_price
