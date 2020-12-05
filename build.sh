7c73f4b040dfb2467eab0ede8a9f834b0242ed74
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2020-12-05 13:16:55 +0200

    [DEPL] 5.4

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="v5.3"
+VERSION="v5.4"
 docker build -t erpukraine/custom:penguin-13.0-${VERSION} . && \
 docker push erpukraine/custom:penguin-13.0-${VERSION}
 # sed -i 's/penguin-13.0-...../penguin-13.0-'${VERSION}'/' ./docker-compose.yml
