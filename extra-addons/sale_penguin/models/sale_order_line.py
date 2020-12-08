4ee94ce796520883a0c0d5f01ba2fae07054656d
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2020-12-08 10:00:37 +0200

    [FIX] use website price as fallback RRP PEN-39

--- extra-addons/sale_penguin/models/sale_order_line.py
+++ extra-addons/sale_penguin/models/sale_order_line.py
@@ -21,30 +21,41 @@ class SaleOrderLine(models.Model):
             )
             line.penguin_net_price_pc = price_info['total_excluded']
 
+    def _get_fallback_rrp_pricelist(self):
+        # We assume that public pricelist for website has retail prices
+        self.ensure_one()
+        order_currency = self.order_id.pricelist_id.currency_id
+        domain = [('currency_id', '=', order_currency.id), ('selectable', '=', True)]
+        return self.env['product.pricelist'].search(domain, limit=1)
+
+    def _get_parent_rrp_pricelist(self):
+        # Return pricelist that is used as base
+        # for current pricelist's formula
+        # or empty record otherwise.
+        self.ensure_one()
+        Pricelist = self.env['product.pricelist']
+        pricelist_id = self.order_id.pricelist_id
+        if not pricelist_id:
+            return Pricelist
+        results = pricelist_id._compute_price_rule([(self.product_id, self.product_uom_qty,
+                                                     self.order_partner_id)])
+        price, suitable_rule = results[product.id]
+        if suitable_rule:
+            item = self.env['product.pricelist.item'].browse(suitable_rule)
+            if item.pricelist_id == pricelist_id and item.base == 'pricelist':
+                return item.base_pricelist_id
+         return Pricelist
+
     @api.depends('order_id.pricelist_id')
     def _compute_penguin_rrp_pc(self):
         for line in self:
-            product = line.product_id
-            pricelist_id = line.order_id.pricelist_id
-                    
-            if not pricelist_id:
-                line.penguin_rrp_pc = line.currency_id._compute(
-                    line.company_id.currency_id, line.currency_id, product.lst_price)
-                continue
-            products_qty_partner = [(
-                product, line.product_uom_qty, line.order_partner_id
-            )]
-            results = pricelist_id._compute_price_rule(products_qty_partner)
-            price, suitable_rule = results[product.id]
-            if suitable_rule:
-                item = self.env['product.pricelist.item'].browse(suitable_rule)
-                if item.pricelist_id == pricelist_id and item.base == 'pricelist':
-                    line.penguin_rrp_pc = line.currency_id._compute(
-                        item.base_pricelist_id.currency_id, line.currency_id,
-                        product.with_context(pricelist=item.base_pricelist_id.id).price)
-                else:
-                    line.penguin_rrp_pc = line.currency_id._compute(
-                        line.company_id.currency_id, line.currency_id, product.lst_price)
-            else:
-                line.penguin_rrp_pc = line.currency_id._compute(
-                    line.company_id.currency_id, line.currency_id, product.lst_price)
+            rrp_pricelist = line._get_parent_rrp_pricelist()
+            if not rrp_pricelist:
+                rrp_pricelist = line._get_fallback_rrp_pricelist()
+            rrp_pc = line.product_id.with_context(pricelist=rrp_pricelist.id).price
+            if line.currency_id != rrp_pricelist.currency_id:
+                # convert to Order currency
+                fx_date = line.order_id.date_order or fields.Date.today()
+                rrp_pc = rrp_pricelist.currency_id._convert(rrp_pc, line.currency_id,
+                                                            line.company_id, fx_date)
+            line.penguin_rrp_pc = rrp_pc
