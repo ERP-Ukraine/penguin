dc7b8839ae1fd5afd94c1e3c501aded4df09f675
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2021-02-17 14:31:46 +0000

    [FIX] invoices: compute rrp based on sale order line

--- extra-addons/account_penguin/models/account_move_line.py
+++ extra-addons/account_penguin/models/account_move_line.py
@@ -4,16 +4,10 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    penguin_net_price_pc = fields.Monetary(string='Net Price/pc', compute='_compute_penguin_net_price_pc')
+    penguin_rrp_pc = fields.Monetary(string='RRP/pc', compute='_compute_penguin_rrp')
+    penguin_net_price_pc = fields.Monetary(string='Net Price/pc', compute='_compute_penguin_rrp')
 
-    def _compute_penguin_net_price_pc(self):
+    def _compute_penguin_rrp(self):
         for line in self:
-            price_info = line.tax_ids.compute_all(
-                line.price_unit * (1 - (line.discount or 0.0) / 100.0),
-                line.always_set_currency_id,  # use currency_id if it's set or company currency
-                1,  # quantity
-                product=line.product_id,
-                partner=line.move_id.partner_id,
-                is_refund=line.move_id.type in ('out_refund', 'in_refund'),
-            )
-            line.penguin_net_price_pc = price_info['total_excluded']
+            line.penguin_rrp_pc = 0
+            line.penguin_net_price_pc = 0
