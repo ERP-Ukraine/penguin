71b6298aa5508cf2c9c774e734bc3a53fac70566
Nikita Borovik <nick.borovik@gmail.com>
2021-03-18 14:38:48 +0200

    [IMP] account_penguin: show amount_residual in account.move list view PEN-48

--- extra-addons/account_penguin/__manifest__.py
+++ extra-addons/account_penguin/__manifest__.py
@@ -1,11 +1,12 @@
 {
     'name': 'Penguin Invoicing',
-    'version': '13.0.1.4',
+    'version': '13.0.1.5',
     'summary': 'Invoices & Payments',
     'category': 'Accounting/Accounting',
     'depends': ['account'],
     'data': [
-        'report/report_invoice.xml'
+        'report/report_invoice.xml',
+        'views/account_move_views.xml'
     ],
     'demo': [],
     'qweb': [],
