ea1f906590cf38295626ad8ba86a4da71c35fbcf
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2021-05-06 17:25:47 +0300

    [DEPL] 7.8

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION="v7.7"
+VERSION="v7.8"
 docker build -t erpukraine/custom:penguin-13.0-${VERSION} . && \
 docker push erpukraine/custom:penguin-13.0-${VERSION}
 # sed -i 's/penguin-13.0-...../penguin-13.0-'${VERSION}'/' ./docker-compose.yml
