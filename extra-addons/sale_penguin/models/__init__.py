cd31b12e815e092347cc4073401f27744bb73ef2
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2021-05-26 09:25:18 +0000

    [IMP] allow to select  portal users on sales team

--- extra-addons/sale_penguin/models/__init__.py
+++ extra-addons/sale_penguin/models/__init__.py
@@ -2,3 +2,4 @@ from . import sale_order
 from . import sale_order_line
 from . import res_users
 from . import account_move
+from . import crm_team
---
+++ extra-addons/sale_penguin/models/crm_team.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class CrmTeam(models.Model):
+    _inherit = 'crm.team'
+
+    # override domain
+    member_ids = fields.One2many(
+        'res.users', 'sale_team_id', string='Channel Members', check_company=True,
+        domain=lambda self: [('is_salesperson', '=', True)],
+        help="Add members to automatically assign their documents to this sales team. You can only be member of one team.")
