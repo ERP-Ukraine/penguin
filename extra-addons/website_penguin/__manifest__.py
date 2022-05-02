bf66ea2ca7a3ee3b4dbe4f7e4fcf9d8884d8eea1
Nikita Borovik <nick.borovik@gmail.com>
2022-05-02 11:29:40 +0300

    [MIG] website_penguin: version 15
    PEN-76

--- extra-addons/website_penguin/__manifest__.py
+++ extra-addons/website_penguin/__manifest__.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Penguin Website',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
     'category': 'Website/Website',
-    'sequence': 7,
     'version': '1.3',
-    'description': "",
-    'depends': [
-        'website'
-    ],
-    'data': [
-        'views/assets.xml',
-        'views/website_templates.xml'
-    ],
-    'demo': [],
-    'installable': False,
+    'sequence': 7,
+    'depends': ['website'],
+    'data': ['views/website_templates.xml'],
+    'assets': {
+        'web._assets_frontend_helpers': [
+            '/website_penguin/static/src/scss/bootstrap_variables.scss',
+        ],
+        'web.assets_frontend': [
+            '/website_penguin/static/src/scss/website.scss'
+        ],
+    },
+    'installable': True,
     'auto_install': True,
+    'application': False,
 }
