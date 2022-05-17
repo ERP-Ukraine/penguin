80500e0dfe4144b7e1a2feeadf1af99e7f4de5a5
Nikita Borovik <nick.borovik@gmail.com>
2022-05-17 16:50:55 +0300

    [FIX] website_sale_penguin: - fix description moving in product page - fix color picker margin - unlink view - template fixes
    PEN-76

--- extra-addons/website_sale_penguin/migrations/15.0.2.5/pre-unlink-views.py
+++ extra-addons/website_sale_penguin/migrations/15.0.2.5/pre-unlink-views.py
@@ -8,7 +8,8 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     view_names = ['website_sale_penguin.website_product_template_form_view',
-                  'website_sale_penguin.penguin_frontend_layout']
+                  'website_sale_penguin.penguin_frontend_layout',
+                  'website_sale_penguin.penguin_product']
     for view_name in view_names:
         view = env.ref(view_name, raise_if_not_found=False)
         if view:
