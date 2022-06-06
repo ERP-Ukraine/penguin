9012bf8b7620dab9a04f16b5fc46f4356b128866
Nikita Borovik <nick.borovik@gmail.com>
2022-06-06 13:10:47 +0300

    [IMP] website_sale_penguin: - colorpicker changes and - pricelist for navbar
    PEN-76

--- extra-addons/website_sale_penguin/__manifest__.py
+++ extra-addons/website_sale_penguin/__manifest__.py
@@ -7,7 +7,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Website/Website',
-    'version': '2.5',
+    'version': '2.6',
     'depends': [
         'website_penguin',
         'website_sale'
