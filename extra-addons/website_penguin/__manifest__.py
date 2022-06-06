8d50fdf6c015eeb4624f4b8f81f2c8da405fac94
Nikita Borovik <nick.borovik@gmail.com>
2022-06-06 13:09:52 +0300

    [IMP] website_penguin: navbar changes
    PEN-76

--- extra-addons/website_penguin/__manifest__.py
+++ extra-addons/website_penguin/__manifest__.py
@@ -6,7 +6,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Website/Website',
-    'version': '1.3',
+    'version': '1.4',
     'sequence': 7,
     'depends': ['website'],
     'data': ['views/website_templates.xml'],
