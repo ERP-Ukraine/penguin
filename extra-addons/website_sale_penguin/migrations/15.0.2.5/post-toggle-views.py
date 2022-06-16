16947ad362fff29c799e398da9ac6ac2c64cb602
Nikita Borovik <nick.borovik@gmail.com>
2022-06-16 15:25:43 +0300

    [IMP] website_sale_penguin: disbable social media buttons
    PEN-79

---
+++ extra-addons/website_sale_penguin/migrations/15.0.2.5/post-toggle-views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import logging
+from odoo import api, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    view_names = ['website_sale.product_share_buttons']
+    for view_name in view_names:
+        view = self.env.ref(view_name, raise_if_not_found=False)
+        if view:
+            _logger.info('Toggling active state for view with id %d to %s' % (view.id, not view.active))
+            view.active = not view.active
