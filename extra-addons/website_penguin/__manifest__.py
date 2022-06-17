a463fef878b8d0739a7bedc0172941b2f90b03ab
Nikita Borovik <nick.borovik@gmail.com>
2022-06-17 13:22:22 +0300

    [IMP] website_penguin: - migration for disabling views - enable penguin footer for website.layout
    PEN-81

--- extra-addons/website_penguin/__manifest__.py
+++ extra-addons/website_penguin/__manifest__.py
@@ -6,7 +6,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Website/Website',
-    'version': '1.5',
+    'version': '1.6',
     'sequence': 7,
     'depends': ['website'],
     'data': [
