78659753490c31716cf505f2f3ab29f0938e28a3
Nikita Borovik <nick.borovik@gmail.com>
2022-06-20 12:22:25 +0300

    [ADD] theme_penguin_default: theme for penguin
    PEN-82

---
+++ extra-addons/theme_penguin_default/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Penguin Default Theme',
+    'category': 'Theme',
+    'author': 'ERP Ukraine LLC',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'data/ir_asset.xml',
+    ],
+    'images': [
+        'static/description/theme_penguin_default_screenshot.jpg',
+    ],
+    'auto_install': True,
+    'installable': True,
+}
