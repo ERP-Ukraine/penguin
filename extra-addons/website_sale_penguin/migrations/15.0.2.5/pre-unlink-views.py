1ed4795aaee762c1d54a35b56b8967cf8d0ce82a
Nikita Borovik <nick.borovik@gmail.com>
2022-06-20 15:12:01 +0300

    [FIX] website_sale_penguin: fix templates
    PEN-82

--- extra-addons/website_sale_penguin/migrations/15.0.2.5/pre-unlink-views.py
+++ extra-addons/website_sale_penguin/migrations/15.0.2.5/pre-unlink-views.py
@@ -8,10 +8,14 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     view_names = ['website_sale_penguin.website_product_template_form_view',
-                  'website_sale_penguin.penguin_frontend_layout',
-                  'website_sale_penguin.penguin_product']
+                  'website_sale_penguin.penguin_frontend_layout']
     for view_name in view_names:
         view = env.ref(view_name, raise_if_not_found=False)
         if view:
             _logger.info('Unlinking view with id %d' % view.id)
             view.unlink()
+
+    views = env['ir.ui.view'].search([('key', '=', 'website_sale_penguin.penguin_product')])
+    if views:
+        _logger.info('Unlinking penguin_product views with ids %s' % view.ids)
+        views.unlink()
