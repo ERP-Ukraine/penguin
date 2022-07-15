6cebb62b82954aec9e1b9dd1f719e5044afe46f8
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2022-07-15 18:55:40 +0000

    [FIX] there is no self

--- extra-addons/website_sale_penguin/migrations/15.0.2.5/post-toggle-views.py
+++ extra-addons/website_sale_penguin/migrations/15.0.2.5/post-toggle-views.py
@@ -9,7 +9,7 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     view_names = ['website_sale.product_share_buttons']
     for view_name in view_names:
-        view = self.env.ref(view_name, raise_if_not_found=False)
+        view = env.ref(view_name, raise_if_not_found=False)
         if view:
             _logger.info('Toggling active state for view with id %d to %s' % (view.id, not view.active))
             view.active = not view.active
