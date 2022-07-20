8b687ea98fb3256037b98c05e571e8829d1831d6
Nikita Borovik <nick.borovik@gmail.com>
2022-07-20 23:22:34 +0300

    [IMP] account_penguin: make invoice report more similar to 13 version
    PEN-78

--- extra-addons/account_penguin/__manifest__.py
+++ extra-addons/account_penguin/__manifest__.py
@@ -1,16 +1,18 @@
 {
-    'name': 'Penguin Invoicing',
-    'version': '1.6',
-    'summary': 'Invoices & Payments',
+    'name': 'Account Penguin',
+    'summary': 'Penguin accounting customizations',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
     'category': 'Accounting/Accounting',
+    'version': '1.7',
     'depends': ['account'],
     'data': [
         'report/report_invoice.xml',
         'views/account_move_views.xml'
     ],
-    'demo': [],
-    'qweb': [],
+    'application': False,
     'installable': True,
     'auto_install': True,
-    'license': 'LGPL-3',
 }
