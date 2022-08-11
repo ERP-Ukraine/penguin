9816045207062bb13593be401bb9fc2574bbd9c4
Nikita Borovik <nick.borovik@gmail.com>
2022-08-11 19:01:59 +0300

    [IMP] sale_penguin: change email template for sale order confirmation
    PEN-92

--- extra-addons/sale_penguin/__manifest__.py
+++ extra-addons/sale_penguin/__manifest__.py
@@ -7,7 +7,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Sales/Sales',
-    'version': '2.5',
+    'version': '2.6',
     'depends': [
         'product_penguin',
         'sale',
