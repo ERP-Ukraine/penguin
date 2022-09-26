068a938609a795828fba114b0ee6bb4662ab4994
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2022-09-26 22:13:41 +0300

    sale_arnoldsports: show chatter PEN-106

--- extra-addons/sale_arnoldsports/__manifest__.py
+++ extra-addons/sale_arnoldsports/__manifest__.py
@@ -7,7 +7,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Sales/Sales',
-    'version': '1.0',
+    'version': '1.1',
     'depends': [
         'sale',
     ],
