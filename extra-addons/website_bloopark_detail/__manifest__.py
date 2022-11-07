540bd19b9bf75f1011367d5fafea9f97a87fc99d
Nikita Borovik <nick.borovik@gmail.com>
2022-11-07 21:18:43 +0200

    [IMP] website_bloopark_detail: product page changes
    PEN-122

--- extra-addons/website_bloopark_detail/__manifest__.py
+++ extra-addons/website_bloopark_detail/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Bloopark Detail page',
-    'version': '15.0.0.7.0',
+    'version': '15.0.0.7.1',
     'author': 'Bloopark systems GmbH & Co. KG',
     'website': 'https://bloopark.de',
     'support': 'support@/bloopark.de',
@@ -9,14 +9,12 @@
     'category': 'Website/Website',
     'sequence': 7,
     'depends': ['website', 'theme_prime'],
-    'data': [
-        'views/layout.xml',
-    ],
-    'images': ['static/description/icon.png',
-    ],
+    'data': ['views/layout.xml'],
+    'images': ['static/description/icon.png'],
     'assets': {
         'web.assets_frontend': [
             'website_bloopark_detail/static/src/scss/style.scss',
+            'website_bloopark_detail/static/src/js/script.js',
         ],
     },
     'installable': True,
