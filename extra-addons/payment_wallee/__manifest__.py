38edc0466e27a64559f786c16138a063c2831d91
Nikita Borovik <nick.borovik@gmail.com>
2023-01-25 10:29:42 +0200

    [FIX] payment_wallee: fix t-set="default_payment_option_id" so that fits wallee default views modifications
    PEN-148

--- extra-addons/payment_wallee/__manifest__.py
+++ extra-addons/payment_wallee/__manifest__.py
@@ -8,7 +8,7 @@
         Payment Acquirer: Wallee.com
         More infos on integrated payment gateways,https://app-wallee.com
     ''',
-    'version': '15.0.1.2',
+    'version': '15.0.1.3',
     'description': """Wallee Payment Acquirer""",
     'currency': 'EUR',
     'price': 150,
