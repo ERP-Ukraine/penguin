73e3ced582442e993f930524a98b9d3f85dc79e2
Nikita Borovik <nick.borovik@gmail.com>
2023-01-25 10:20:05 +0200

    [FIX] payment_transfer_confirmation: fix mistake for overriding method _reconcile_after_done for transaction
    PEN-148

--- extra-addons/payment_transfer_confirmation/models/payment_transaction.py
+++ extra-addons/payment_transfer_confirmation/models/payment_transaction.py
@@ -2,6 +2,7 @@
 from collections import defaultdict
 
 from odoo import models
+from odoo.addons.sale.models.payment_transaction import PaymentTransaction
 
 
 class TransferPaymentTransaction(models.Model):
@@ -20,10 +21,29 @@ class TransferPaymentTransaction(models.Model):
         else:
             self._set_done()
 
+    # overriden from module 'sale' because there is no need to create
+    # payment for wire transfer transactions it has to be created manually
     def _reconcile_after_done(self):
-        if self.provider != 'transfer':
-            return super()._reconcile_after_done()
-        self.invoice_ids.filtered(lambda inv: inv.state == 'draft').action_post()
+        """ Override of payment to automatically confirm quotations and generate invoices. """
+        draft_orders = self.sale_order_ids.filtered(lambda so: so.state in ('draft', 'sent'))
+        for tx in self:
+            tx._check_amount_and_confirm_order()
+        confirmed_sales_orders = draft_orders.filtered(lambda so: so.state in ('sale', 'done'))
+        # send order confirmation mail
+        confirmed_sales_orders._send_order_confirmation_mail()
+        # invoice the sale orders if needed
+        self._invoice_sale_orders()
+
+        # ERPU custom code
+        if self.provider == 'transfer':
+            res = self.invoice_ids.filtered(lambda inv: inv.state == 'draft').action_post()
+        else:
+            res = super(PaymentTransaction, self)._reconcile_after_done()
+        # ERPU end of custom code
+
+        if self.env['ir.config_parameter'].sudo().get_param('sale.automatic_invoice') and any(so.state in ('sale', 'done') for so in self.sale_order_ids):
+            self.filtered(lambda t: t.sale_order_ids.filtered(lambda so: so.state in ('sale', 'done')))._send_invoice()
+        return res
 
     def _send_capture_request(self):
         super()._send_capture_request()
