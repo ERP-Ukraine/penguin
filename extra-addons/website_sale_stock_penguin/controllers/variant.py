fef0dfa03e386ed384242a1ad9dc3f763d372bf3
Nikita Borovik <nick.borovik@gmail.com>
2023-03-08 17:52:37 +0200

    [FIX] website_sale_stock_penguin: do not look for forecast qty

--- extra-addons/website_sale_stock_penguin/controllers/variant.py
+++ extra-addons/website_sale_stock_penguin/controllers/variant.py
@@ -4,8 +4,8 @@ from odoo.http import request
 from odoo.addons.sale.controllers.variant import VariantController
 
 
-def filter_product_virtual_available(product):
-    return product.website_published and product.virtual_available > 0 and product.free_qty > 0
+def filter_product_available_on_hand(product):
+    return product.website_published and product.free_qty > 0
 
 
 class WebsiteSaleStockPenguinVariantController(VariantController):
@@ -15,5 +15,5 @@ class WebsiteSaleStockPenguinVariantController(VariantController):
         pt = request.env['product.template'].sudo().with_context(warehouse=warehouse_ids).browse(int(product_template_id))
         ptav = request.env['product.template.attribute.value'].browse(int(product_template_attribute_value_id))
         product_variants_color = pt._get_possible_variants().filtered(lambda p: ptav in p.product_template_variant_value_ids)
-        available_sizes = product_variants_color.filtered(filter_product_virtual_available)
+        available_sizes = product_variants_color.filtered(filter_product_available_on_hand)
         return available_sizes.product_template_attribute_value_ids.ids
