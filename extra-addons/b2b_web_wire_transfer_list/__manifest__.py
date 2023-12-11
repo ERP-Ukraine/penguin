1d94423ea72a3ab14c03a3c280ca2ae24e2fa150
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-12-11 10:09:03 +0000

    [ADD] b2b_web_wire_transfer_list: b2b filter for payment
    PEN-206

---
+++ extra-addons/b2b_web_wire_transfer_list/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'B2B web wire transfer list',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'version': '1.0',
+    'category': 'Other',
+    'license': 'LGPL-3',
+    'depends': ['website_sale'],
+    'data': [
+        'views/payment_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
