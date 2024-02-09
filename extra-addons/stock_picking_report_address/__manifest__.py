ff9299491ed8b8fc7e80bc0eea416ef464e923db
Nazar Hladaniuk <powerlolek46@gmail.com>
2024-02-09 15:18:56 +0000

    [ADD] stock_picking_report_address: show partner address
    pen-210

---
+++ extra-addons/stock_picking_report_address/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Stock picking report address',
+    'author': 'ERP Ukraine',
+    'website': 'https://erp.co.ua',
+    'support': 'support@erp.co.ua',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'version': '1.0',
+    'depends': ['stock'],
+    'data': [
+        'report/report_deliveryslip.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
