e52b2d61adc8f0ed71ac9a8b3b6b724ef0350004
Nazar Hladaniuk <powerlolek46@gmail.com>
2024-02-13 11:30:31 +0000

    [IMP] email_style_changes: remove sent by
    PEN-220

--- extra-addons/email_style_changes/__manifest__.py
+++ extra-addons/email_style_changes/__manifest__.py
@@ -5,7 +5,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Other',
-    'version': '1.0',
+    'version': '1.1',
     'depends': ['mail', 'purchase'],
     'data': [
         'data/mail_templates.xml',
