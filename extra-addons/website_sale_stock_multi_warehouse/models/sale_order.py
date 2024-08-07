a9ecc5ac7545af2889ad232a7897e6630a41cfd5
Nazar Hladaniuk <powerlolek46@gmail.com>
2024-08-07 12:07:00 +0000

    [MIG] website_sale_stock_multi_warehouse: 17.0
    PEN-213

--- extra-addons/website_sale_stock_multi_warehouse/models/sale_order.py
+++ extra-addons/website_sale_stock_multi_warehouse/models/sale_order.py
@@ -1,29 +1,22 @@
 from odoo import _, models
 
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # override for enable products from multiple warehouses
-    def _cart_lines_stock_update(self, values, **kwargs):
-        line_id = values.get('line_id')
-        for line in self.order_line:
-            if line.product_id.type == 'product' and not line.product_id.allow_out_of_stock_order:
-                cart_qty = sum(self.order_line.filtered(lambda p: p.product_id.id == line.product_id.id).mapped('product_uom_qty'))
-                # ERPUkraine use multiple warehouses
-                warehouse_ids = self.sudo().company_id.warehouse_ids
-                context = dict(warehouse=warehouse_ids.ids)
-                available_qty = line.product_id.with_context(context).free_qty
-                if (line_id == line.id) and cart_qty > available_qty:
-                    qty = available_qty - cart_qty
-                    new_val = super(SaleOrder, self.with_context(context))._cart_update(line.product_id.id, line.id, qty, 0, **kwargs)
-                    values.update(new_val)
-
-                    # Make sure line still exists, it may have been deleted in super()._cart_update because qty can be <= 0
-                    if line.exists() and new_val['quantity']:
-                        line.warning_stock = _('You ask for %s products but only %s is available') % (cart_qty, new_val['quantity'])
-                        values['warning'] = line.warning_stock
-                    else:
-                        self.warning_stock = _("Some products became unavailable and your cart has been updated. We're sorry for the inconvenience.")
-                        values['warning'] = self.warning_stock
-        return values
+    def _get_cart_and_free_qty(self, product, line=None):
+        self.ensure_one()
+        if not line and not product:
+            return 0, 0
+        cart_qty = sum(self._get_common_product_lines(line, product).mapped('product_uom_qty'))
+        # ERPUkraine use multiple warehouses
+        free_qty = 0
+        warehouse_ids =  self.sudo().company_id.warehouse_ids # line.company_id.warehouse_ids
+        for warehouse_id in warehouse_ids:
+            free_qty += (product or line.product_id).with_context(warehouse=warehouse_id.id).free_qty
+        #
+        return cart_qty, free_qty
