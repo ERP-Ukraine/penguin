edb71634007e5606cbdda2f4157d4aa459d502de
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2025-06-28 11:49:58 +0300

    delete google_shopping_feed_gcs

--- extra-addons/google_shopping_feed_gcs/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import google_shopping_api
-from . import models
-from . import controllers
-from . import wizard
--- extra-addons/google_shopping_feed_gcs/google_shopping_api/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/python3
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from .import google_shopping_api
