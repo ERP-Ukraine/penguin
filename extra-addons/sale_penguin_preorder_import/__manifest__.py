c6608755e93e8c9e2360075f326d3a2007cb6f6d
tsehelnyk-yehor <yehor.tsehelnyk@erp.co.ua>
2025-07-18 11:16:44 +0300

    [FIX] new_products_import, sale_penguin_preorder_import: fix import xlsx files PEN-368

--- extra-addons/sale_penguin_preorder_import/__manifest__.py
+++ extra-addons/sale_penguin_preorder_import/__manifest__.py
@@ -7,7 +7,7 @@
     'support': 'support@erp.co.ua',
     'license': 'LGPL-3',
     'category': 'Sales/Sales',
-    'version': '1.2',
+    'version': '1.3',
     'depends': [
         'base_import',
         'sale_penguin',
