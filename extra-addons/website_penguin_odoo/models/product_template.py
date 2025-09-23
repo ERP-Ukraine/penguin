9d8dbb8410b8f27dbdfd81b694570632f7ab2109
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2025-09-23 15:07:46 +0300

    [UPLOAD] website_penguin_odoo

---
+++ extra-addons/website_penguin_odoo/models/product_template.py
@@ -0,0 +1,17 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    def _get_own_attribute_exclusions(self, combination_ids=None):
+        self.ensure_one()
+        vals = super()._get_own_attribute_exclusions(combination_ids)
+        product_template_attribute_values = self.valid_product_template_attribute_line_ids.product_template_value_ids
+
+        for ptav in product_template_attribute_values:
+            for var in ptav.ptav_product_variant_ids:
+                if var.virtual_available < 1:
+                    vals[ptav.id] += [item for item in var.product_template_attribute_value_ids.ids if item not in ptav.ids]
+                    
+        return vals
---
+++ extra-addons/website_penguin_odoo/static/src/js/product_variant.js
@@ -0,0 +1,27 @@
+/** @odoo-module **/
+
+import { WebsiteSale } from '@website_sale/js/website_sale';
+import { _t } from "@web/core/l10n/translation";
+import { rpc } from "@web/core/network/rpc";
+
+WebsiteSale.include({
+    
+    onChangeVariant: function (ev) {
+        this._super.apply(this, arguments);
+        let $parent = $(ev.target).closest('.js_product');
+        let title = $parent.find("[data-attribute_display_type='color']").find(".active").find("input")[0].dataset['value_name'];
+        let elem = $parent.find("[data-attribute_display_type='color']").find(".attribute_name")[0].innerText;
+        $parent.find("[data-attribute_display_type='color']").find(".attribute_name")[0].innerText = elem.split(' :')[0] + " : " + title;
+    },
+
+    _toggleDisable: function ($parent, isCombinationPossible) {
+        if(isCombinationPossible){
+            $parent.find("#o_wsale_cta_wrapper").removeClass("d-none");
+            $parent.find(".css_not_available_msg")[0].style.display = "none"
+
+        }else{
+            $parent.find("#o_wsale_cta_wrapper").addClass("d-none");
+            $parent.find(".css_not_available_msg")[0].style.display = "block"
+        }
+    },
+});
